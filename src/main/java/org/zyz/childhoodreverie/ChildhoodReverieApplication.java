b8b24ced74e101ecd3fe60d45063737b987600a7
SilverIceKey <z516798599@gmail.com>
2025-06-22 13:55:15 +0800

    first init

---
+++ src/main/java/org/zyz/childhoodreverie/ChildhoodReverieApplication.java
@@ -0,0 +1,13 @@
+package org.zyz.childhoodreverie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildhoodReverieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildhoodReverieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zyz/childhoodreverie/ChildhoodReverieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zyz.childhoodreverie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildhoodReverieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
